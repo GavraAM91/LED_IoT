1c1d13d17502a5e1cb2e7d420fbd89bfc8b78ee5
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-07-20 10:57:37 +0700

    setup blynk library

---
+++ Edgent_ESP8266.ino
@@ -0,0 +1,32 @@
+/* Fill in information from your Blynk Template here */
+/* Read more: https://bit.ly/BlynkInject */
+#define BLYNK_TEMPLATE_ID "TMPL6SA9Gp5Jq"
+#define BLYNK_TEMPLATE_NAME "NodeMCU"
+
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+
+#define BLYNK_PRINT Serial
+//#define BLYNK_DEBUG
+
+#define APP_DEBUG
+
+// Uncomment your board, or configure a custom board in Settings.h
+//#define USE_SPARKFUN_BLYNK_BOARD
+//#define USE_NODE_MCU_BOARD
+//#define USE_WITTY_CLOUD_BOARD
+//#define USE_WEMOS_D1_MINI
+
+#include "BlynkEdgent.h"
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(100);
+
+  BlynkEdgent.begin();
+}
+
+void loop() {
+  BlynkEdgent.run();
+}
+
